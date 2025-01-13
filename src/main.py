77edc9f4506a3e237b01b3de5617401977794786
Marlo Swora <sworam@uni-hildesheim.de>
2025-01-13 23:01:53 +0100

    add copy_recursively()

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_recursively(src: str, dest: str) -> None:
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.makedirs(dest)
+    objects = os.listdir(src)
+    for obj in objects:
+        src_path = os.path.join(src, obj)
+        dst_path = os.path.join(dest, obj)
+        if os.path.isfile(src_path):
+            print(f"copying file {obj} from {src} to {dest}")
+            shutil.copy(src_path, dst_path)
+        elif os.path.isdir(src_path):
+            print(f"copying directory {obj} from {src} to {dest}")
+            copy_recursively(src_path, dst_path)
+
 
 
 def main():
-    textnode = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(textnode)
+    copy_recursively("static", "public")
 
 if __name__ == "__main__":
     main()
